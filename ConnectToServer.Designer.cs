d93ce1074f6f787195d9b7f228f2e0910054a87b
RJ_Infinity <rjinfinity2004@gmail.com>
2023-03-17 12:36:41 +0000

    removed the unused name box

--- ConnectToServer.Designer.cs
+++ ConnectToServer.Designer.cs
@@ -33,8 +33,6 @@
             this.IPAdressLabel = new System.Windows.Forms.Label();
             this.PortLabel = new System.Windows.Forms.Label();
             this.PortInput = new System.Windows.Forms.NumericUpDown();
-            this.NameLabel = new System.Windows.Forms.Label();
-            this.NameInput = new System.Windows.Forms.TextBox();
             this.ButtonPanel = new System.Windows.Forms.Panel();
             this.LaunchButton = new System.Windows.Forms.Button();
             this.ErrorMessageToolTip = new System.Windows.Forms.ToolTip(this.components);
@@ -44,9 +42,9 @@
             // 
             // IPAdressInput
             // 
-            this.IPAdressInput.Location = new System.Drawing.Point(73, 12);
+            this.IPAdressInput.Location = new System.Drawing.Point(80, 12);
             this.IPAdressInput.Name = "IPAdressInput";
-            this.IPAdressInput.Size = new System.Drawing.Size(196, 23);
+            this.IPAdressInput.Size = new System.Drawing.Size(189, 23);
             this.IPAdressInput.TabIndex = 0;
             // 
             // IPAdressLabel
@@ -54,9 +52,9 @@
             this.IPAdressLabel.AutoSize = true;
             this.IPAdressLabel.Location = new System.Drawing.Point(12, 15);
             this.IPAdressLabel.Name = "IPAdressLabel";
-            this.IPAdressLabel.Size = new System.Drawing.Size(55, 15);
+            this.IPAdressLabel.Size = new System.Drawing.Size(62, 15);
             this.IPAdressLabel.TabIndex = 1;
-            this.IPAdressLabel.Text = "IP Adress";
+            this.IPAdressLabel.Text = "IP Address";
             // 
             // PortLabel
             // 
@@ -79,40 +77,22 @@
             this.PortInput.Size = new System.Drawing.Size(222, 23);
             this.PortInput.TabIndex = 3;
             // 
-            // NameLabel
-            // 
-            this.NameLabel.AutoSize = true;
-            this.NameLabel.Location = new System.Drawing.Point(12, 73);
-            this.NameLabel.Name = "NameLabel";
-            this.NameLabel.Size = new System.Drawing.Size(39, 15);
-            this.NameLabel.TabIndex = 4;
-            this.NameLabel.Text = "Name";
-            // 
-            // NameInput
-            // 
-            this.NameInput.Location = new System.Drawing.Point(57, 70);
-            this.NameInput.Name = "NameInput";
-            this.NameInput.Size = new System.Drawing.Size(212, 23);
-            this.NameInput.TabIndex = 5;
-            // 
             // ButtonPanel
             // 
             this.ButtonPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ButtonPanel.Controls.Add(this.IPAdressLabel);
             this.ButtonPanel.Controls.Add(this.IPAdressInput);
             this.ButtonPanel.Controls.Add(this.PortLabel);
-            this.ButtonPanel.Controls.Add(this.NameInput);
             this.ButtonPanel.Controls.Add(this.PortInput);
-            this.ButtonPanel.Controls.Add(this.NameLabel);
             this.ButtonPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.ButtonPanel.Location = new System.Drawing.Point(0, 0);
             this.ButtonPanel.Name = "ButtonPanel";
-            this.ButtonPanel.Size = new System.Drawing.Size(277, 105);
+            this.ButtonPanel.Size = new System.Drawing.Size(277, 82);
             this.ButtonPanel.TabIndex = 6;
             // 
             // LaunchButton
             // 
-            this.LaunchButton.Location = new System.Drawing.Point(190, 117);
+            this.LaunchButton.Location = new System.Drawing.Point(190, 94);
             this.LaunchButton.Name = "LaunchButton";
             this.LaunchButton.Size = new System.Drawing.Size(75, 23);
             this.LaunchButton.TabIndex = 0;
@@ -130,7 +110,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(277, 152);
+            this.ClientSize = new System.Drawing.Size(277, 129);
             this.Controls.Add(this.ButtonPanel);
             this.Controls.Add(this.LaunchButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -150,8 +130,6 @@
         private Label IPAdressLabel;
         private Label PortLabel;
         private NumericUpDown PortInput;
-        private Label NameLabel;
-        private TextBox NameInput;
         private Panel ButtonPanel;
         private Button LaunchButton;
         private ToolTip ErrorMessageToolTip;
